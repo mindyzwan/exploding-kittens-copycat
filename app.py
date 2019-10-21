e0c6b7fff186c47726af879192a66b619bf87f38
mindyzwan <mindyzwan@gmail.com>
2019-10-20 18:43:23 -0700

    Add defuse and pair capabilities

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 
 # NEXT TO DO:
-
-# Stop explosion if have defuse
 # Add shuffle capability
 # Add see the future capability
 # Add attack capability
@@ -10,6 +8,7 @@
 
 
 import random
+import os
 
 class Card():
   types = {
@@ -51,14 +50,14 @@ class Card():
     },
     'exploding_kitten': {
       'name': 'Exploding Kitten',
-      'phrase': ['*gnaw, gnaw, gnaw* (on electrical wires)', 'zzzz (warp core + cat hair = BAD)', '*scampers across nuke detonation button*', '*crunch munch* on TNT'],
+      'phrase': ['*gnaw, gnaw, gnaw* (on electrical wires, subsequently blowing up the house)', '\'zzzz\' (cat fell asleep on the warp core! Warp core + cat hair = BAD)', '*cat scampers across nuke detonation button*', '*cat muches on TNT*'],
       'type': 'exploding_kitten',
       'start_count': 0
     },
     'skip': {
       'name': 'Skip',
       'phrase': ['Crab walk with some crabs', 'Engage the hypergoat', 'Don a portable cheetah butt', 'Commandeer a bunnyraptor'],
-      'type': ['skip'],
+      'type': 'skip',
       'start_count': 4
     }
   }
@@ -69,7 +68,7 @@ class Card():
     self.function = self.types[value]['type']
 
   def __str__(self):
-    return f'{self.name} \"{self.phrase}\"'
+    return f'{self.name}'
 
   def __repr__(self):
     return f'{self.name}'
@@ -126,7 +125,7 @@ class Human(Player):
 class Computer(Player):
   def __init__(self, hand):
     super().__init__(hand)
-    self.name = random.choice(['R2D2', 'C-3PO', 'HAL', 'GWB-666', 'Alexa', 'Siri', 'Computer', 'Cortana'])
+    self.name = random.choice(['R2D2', 'C-3PO', 'HAL', 'GWB-666', 'Alexa', 'Siri', 'Cortana'])
     print(f'You\'ll be playing against {self.name}.')
     self.species = 'Computer'
   
@@ -140,6 +139,7 @@ class Hand():
     self.deck.deal_starting_hand(self)
 
   def show_cards(self):
+    print('---------------------------')
     print('Your Hand:')
     card_number = 1
     for card in self.cards:
@@ -153,31 +153,90 @@ class Hand():
   def play_card(self, card_index):
     self.cards.pop(card_index)
 
+  def steal_card(self, stolen_card):
+    self.cards.append(stolen_card)
+
+  def get_card_stolen(self, card_index):
+    return self.cards.pop(card_index)
+
 class Turn():
-  def __init__(self, player, game):
+  def __init__(self, player, opponent, game):
     self.skip_turn = False
     self.player = player
     self.game = game
+    self.opponent = opponent
+    print(f'{player.name.upper()}\'S TURN\n')
+
+  def activate_card(self, card_index):
+    card = self.player.hand.cards[card_index]
+    
+    if card.function == 'skip':
+      self.skip_turn = True
+      self.player.hand.play_card(card_index)
+    elif card.function == 'pair_card':
+      if self.check_pair_exist(card): self.play_pair(card_index)
+
+  def check_pair_exist(self, first_card):
+    pair_count = 0
+    for card in self.player.hand.cards:
+      if card.name == first_card.name: pair_count += 1
+      if pair_count >= 2: return True
+    return False
+
+  def play_pair(self, first_card_index):
+    hand = self.player.hand
+    hand.play_card(first_card_index)
+    first_card = hand.cards[first_card_index]
+
+    second_card_index = 0
+    for card in hand.cards:
+      if card.name == first_card.name: break
+      second_card_index += 1
+
+    hand.play_card(second_card_index)
+    print(f'\nA pair has been found! {self.player.name} gets to steal a card.')
+    self.steal_card()
+
+  def steal_card(self):
+    opponent_hand = self.opponent.hand
+    card_index = random.randint(0, len(opponent_hand.cards) - 1)
+    stolen_card = opponent_hand.get_card_stolen(card_index)
+    print(f'Stolen card: {stolen_card}')
+    self.player.hand.steal_card(stolen_card)
+
+  def check_for_defuse_card(self, game):
+    card_index = 0
+
+    for card in self.player.hand.cards:
+      if card.name == 'Defuse':
+        game.exploded = False
+        card_phrase = self.player.hand.cards[card_index].phrase
+        self.player.hand.play_card(card_index)
+        print(f'\t{self.player.name} DEFUSED THE KITTEN, {card_phrase}\n\n')
+      card_index += 1
 
   def end_turn(self, game):
     self.player.hand.draw_new_card()
     if self.player.species == 'Human':
-      print(f'You drew: {self.player.hand.cards[-1]}')
+      input(f'\nPress enter to finish your turn by drawing a card. ')
+      print(f'\n\t> You drew: {self.player.hand.cards[-1]}\n\n\n')
     else:
-      print(f'- {self.player.name} drew a card -')
+      print(f'- {self.player.name} drew a card -\n')
     if self.player.hand.cards[-1].name == 'Exploding Kitten':
       game.exploded = True
-    print('')
+    if game.exploded:
+      self.check_for_defuse_card(game)
+
 
 class ComputerTurn(Turn):
-  def __init__(self, player, game):
-    super().__init__(player, game)
+  def __init__(self, player, opponent, game):
+    super().__init__(player, opponent, game)
     self.end_turn(game)
 
 
 class HumanTurn(Turn):
-  def __init__(self, player, game):
-    super().__init__(player, game)
+  def __init__(self, player, opponent, game):
+    super().__init__(player, opponent, game)
     self.player = player
     self.game = game
 
@@ -186,16 +245,18 @@ class HumanTurn(Turn):
 
   def play_cards(self):
     self.player.hand.show_cards()
-
     answer = input(f'Would you like to play a card? (y/n) ')
     play_card = (answer == 'y')
 
     while play_card:
-      card_choice = self.choose_card()
-      self.activate_card(self.player.hand.cards[card_choice])
-      self.player.hand.play_card(card_choice)
+      card_index = self.choose_card()
+      self.activate_card(card_index)
+      os.system('cls||clear')
+      self.player.hand.show_cards()
+
       answer = input(f'Would you like to play another card? (y/n) ')
       play_card = (answer == 'y')
+      
 
 
   def choose_card(self):
@@ -206,13 +267,10 @@ class HumanTurn(Turn):
 
     return choice
 
-  def activate_card(self, card):
-    self.skip_turn = ('skip' in card.function)
-
-
 class Game():
   def __init__(self):
     self.deck = Deck()
+    os.system('cls||clear')
     print('Welcome to Exploding Kittens!')
 
     self.human_hand = Hand(self.deck)
@@ -222,24 +280,32 @@ class Game():
     self.computer = Computer(self.computer_hand)
 
     self.exploded = False
+    input(f'\n\nPress enter to continue  ')
+    os.system('cls||clear')
+
 
   def list_rules(self):
     pass
 
   def play(self):
-    self.deck.insert_exploding_kitten_cards()
-    self.deck.insert_remaining_defuse_cards()
+    for _ in range(5):
+      self.deck.insert_exploding_kitten_cards()
 
     while not self.exploded:
       for player in [self.human, self.computer]:
         if player.species == 'Human':
-          HumanTurn(player, self)
+          HumanTurn(player, self.computer, self)
         else:
-          ComputerTurn(player, self)
+          ComputerTurn(player, self.computer, self)
 
         if self.exploded: 
-          print(f'{player.name} lost!')
+          print(player.hand.cards[-1].phrase)
+          print('\n\nBOOOOOOOOOOMMMMMMM!!!!!\n\n')
+          print(f'{player.name} lost!\n\n')
           break
+        
+        input(f'Press enter to continue')
+        os.system('cls||clear')
 
 
 new_game = Game()
